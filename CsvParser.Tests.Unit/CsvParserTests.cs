4eb6b0ee3cebcc3ee91f407f4e70d02be0f798d4
Dominik Karwowski <karwowski.d@o2.pl>
2020-09-07 21:44:27 +0200

    ADD CORE FUNCTIONALITY
    CsvParser core class was created. Functionality allows for parsing xml file and translate the read data to the data object according to provided mapping function.
    
    Additionally:
    * xml documentation was created
    * several unit tests added in a separate project

---
+++ CsvParser.Tests.Unit/CsvParserTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DjK.Utilities;
+
+namespace Utilities.Tests.Unit
+{
+    [TestFixture]
+    public class CsvParserTests
+    {
+        [Test]
+        public void CsvParser_ConstructedWithoutSpecifyingSeparator_CreatesInstanceWithSemicolonSeparatorByDefault()
+        {
+            // Arrange
+            const string DUMMY_FILE_PATH = @"C:\dummyPath\dummyFile.csv";
+            const char DEFAULT_SEPARATOR = ';';
+
+            // Act
+            var csvParserSut = new CsvParser(DUMMY_FILE_PATH);
+
+            // Assert
+            Assert.AreEqual(DEFAULT_SEPARATOR, csvParserSut.Separators[0]);
+        }
+
+        [Test]
+        public void CsvParser_SourceFilePropertyValue_IsSetFromConstructor()
+        {
+            // Arrange
+            const string DUMMY_FILE_PATH = @"C:\dummyPath\dummyFile.csv";
+
+            // Act
+            var csvParserSut = new CsvParser(DUMMY_FILE_PATH);
+
+            // Assert
+            Assert.AreEqual(DUMMY_FILE_PATH, csvParserSut.SourceFile);
+        }
+
+        [Test]
+        public void Separators_NullValueAssigned_ThrowsArgumentNullExceptio()
+        {
+            // Arrange
+            const string DUMMY_FILE_PATH = @"C:\dummyPath\dummyFile.csv";
+            var csvParserSut = new CsvParser(DUMMY_FILE_PATH);
+
+            // Act
+            void codeToTest() => csvParserSut.Separators = null;
+
+            // Arrange
+            Assert.Throws(
+                Is.TypeOf<ArgumentNullException>()
+                .And.Property("ParamName").EqualTo(nameof(csvParserSut.Separators)),
+                codeToTest);
+        }
+
+        [Test]
+        public void Separators_EmptyArrayAssigned_ThrowsArgumentNullExceptio()
+        {
+            // Arrange
+            const string DUMMY_FILE_PATH = @"C:\dummyPath\dummyFile.csv";
+            var csvParserSut = new CsvParser(DUMMY_FILE_PATH);
+
+            // Act
+            void codeToTest() => csvParserSut.Separators = new char[] { };
+
+            // Arrange
+            Assert.Throws(
+                Is.TypeOf<ArgumentException>().
+                And.Property("ParamName").EqualTo(nameof(csvParserSut.Separators)),
+                codeToTest);
+        }
+    }
+}
---
+++ CsvParser/CsvParser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DjK.Utilities
+{
+    /// <summary>
+    /// Provides a functionality to parse csv file and convert text data to specified data object.
+    /// </summary>
+    public class CsvParser
+    {
+        private char[] _Separators;
+
+        /// <summary>
+        /// Path to csv file.
+        /// </summary>
+        public string SourceFile { get; set; }
+
+        /// <summary>
+        /// Separators used in csv file. Cannot be empty.
+        /// </summary>
+        public char[] Separators
+        {
+            get { return _Separators; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(Separators), "Separators array reference cannot be null.");
+                else if (value.Length == 0)
+                    throw new ArgumentException("At least one separator must be specified.", nameof(Separators));
+                else
+                    _Separators = value;
+            }
+        }
+
+        /// <summary>
+        /// Specify path to csv file. Semicolon (;) will be set as a default separator.
+        /// </summary>
+        /// <param name="file">Path to csv file.</param>
+        public CsvParser(string file)
+            : this(file, ';')
+        { }
+
+        /// <summary>
+        /// Specify path to csv file and a csv separator.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="separator"></param>
+        public CsvParser(string file, char separator)
+            : this(file, new char[] { separator })
+        { }
+
+        /// <summary>
+        /// Specify path to csv file and multiple csv separators.
+        /// </summary>
+        /// <param name="file">Path to csv file.</param>
+        /// <param name="separators">Separators used in csv file.</param>
+        public CsvParser(string file, char[] separators)
+        {
+            SourceFile = file;
+            Separators = separators;
+        }
+
+        /// <summary>
+        /// Converts csv line to data object of type T based on provided mapping function.
+        /// Throws IOException if the specified source file does not exist.
+        /// </summary>
+        /// <typeparam name="T">Data object to be created from the data in csv file.</typeparam>
+        /// <param name="lineToObjectMapper">Function mapping csv line to a specified object.</param>
+        /// <returns>List of data objects created from csv data.</returns>
+        public List<T> LinesToList<T>(Func<string[], T> lineToObjectMapper)
+        {
+            List<T> retrievedData = new List<T>() { };
+
+            if (File.Exists(SourceFile))
+            {
+                using (StreamReader file = new StreamReader(SourceFile))
+                {
+                    string line;
+                    while((line = file.ReadLine()) != null)
+                    {
+                        string[] lineElements = line.Split(Separators);
+                        T objectFromLine = lineToObjectMapper(lineElements);
+                        if (objectFromLine != null) retrievedData.Add(objectFromLine);
+                    }
+                }
+            }
+            else
+            {
+                throw new IOException("Input file not found.");
+            }
+
+            return retrievedData;
+        }
+    }
+}
